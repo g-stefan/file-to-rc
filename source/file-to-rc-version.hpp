c979c4da5e080fe4bcbd40f3e15f5ffe6a419588
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:19:49 +0300

    update

--- source/file-to-rc-version.hpp
+++ source/file-to-rc-version.hpp
@@ -10,10 +10,10 @@
 #ifndef FILE_TO_RC_VERSION_HPP
 #define FILE_TO_RC_VERSION_HPP
 
-#define FILE_TO_RC_VERSION_ABCD                1,5,0,7
-#define FILE_TO_RC_VERSION_STR                 "1.5.0"
-#define FILE_TO_RC_VERSION_STR_BUILD           "7"
-#define FILE_TO_RC_VERSION_STR_DATETIME        "2021-03-15 12:30:20"
+#define FILE_TO_RC_VERSION_ABCD                1,6,0,8
+#define FILE_TO_RC_VERSION_STR                 "1.6.0"
+#define FILE_TO_RC_VERSION_STR_BUILD           "8"
+#define FILE_TO_RC_VERSION_STR_DATETIME        "2021-07-06 15:11:43"
 
 #ifndef XYO_RC
 
