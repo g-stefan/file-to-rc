42f17d5ac4e621d7d43bb6af9408d60e5a13caaa
Grigore Stefan <g_stefan@yahoo.com>
2023-01-09 15:34:15 +0200

    Update

--- source/XYO/FileToRC.Application/License.cpp
+++ source/XYO/FileToRC.Application/License.cpp
@@ -1,5 +1,5 @@
 // File to RC
-// Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2007-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
@@ -9,11 +9,11 @@
 namespace XYO::FileToRC::Application::License {
 
 	const char *license() {
-		return XYO::ManagedMemory::License::license();
+		return XYO::FileToRC::License::license();
 	};
 
 	const char *shortLicense() {
-		return XYO::ManagedMemory::License::shortLicense();
+		return XYO::FileToRC::License::shortLicense();
 	};
 
 };
--- source/XYO/FileToRC/Version.rh
+++ source/XYO/FileToRC/Version.rh
@@ -1,5 +1,5 @@
 // File to RC
-// Copyright (c) 2022 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2007-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: MIT
